f54ea24e4b0bd5c15b75287f2abe986943089f88
Akkyreuh <louisbenel12@gmail.com>
2023-04-05 12:08:34 +0200

    tournois v1

--- app/Http/Controllers/TOURNOISController.php
+++ app/Http/Controllers/TOURNOISController.php
@@ -3,14 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\TOURNOIS;
+use App\Models\Tournois;
+use Validator;
 
-class TOURNOISController extends Controller{
+class TournoisController extends Controller {
 
-    public function list(Request $request)
-    {
-     $tournois = TOURNOIS::select("idtournois", "jeux", "equipe")->get();          
-    return response()->json($tournois);
-    }
+//Liste tous les tournois
+public function ListeTournois(Request $request)
+{
+$tournois = Tournois::select('idtournois','jeux','nom_tournois')->get();
+return response()->json($tournois);
+}
+
+//  AJOUTER UN TOURNOIS
+
+public function AjouterTournois(Request $request) {
+
+    $validator = Validator::make($request->all(),[        
+        'nom_tournois' => 'required|string',
+        'jeux' => 'required|string',
+         ]);
+
+         if ($validator->fails()) {
+            return response()->json(["status" => 0, "message" => $validator->errors()],400);
+            }
+
+    $tournois = new Tournois;
+    
+    $tournois->nom_tournois = $request->nom_tournois;
+    $tournois->jeux = $request->jeux;
+    $ok = $tournois->save();
+        if ($ok) {
+    return response()->json(["status" => 1, "message" => "Le tournois ajouté"],201);
+        } else {
+        return response()->json(["status" => 0, "message" => "pb lors de
+        l'ajout"],400);
+        }
+            
+
+
+}
+
 
 }
