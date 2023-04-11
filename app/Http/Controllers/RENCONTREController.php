f95cb8cefce8f6284a7396d97b0b434ae0677e36
Akkyreuh <louisbenel12@gmail.com>
2023-04-12 00:51:35 +0200

    modif rencontremodel

--- app/Http/Controllers/RENCONTREController.php
+++ app/Http/Controllers/RENCONTREController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Rencontre;
+use App\Models\RENCONTRE;
 use Validator;
 
 class RENCONTREController extends Controller {
