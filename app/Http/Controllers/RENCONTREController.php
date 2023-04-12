7cf3458da7c3daf2fd2e136c0ea66034075efb36
Akkyreuh <louisbenel12@gmail.com>
2023-04-12 02:51:57 +0200

    LISTE RENCONTRE QUI MARCHE LETSGOOOOOO

--- app/Http/Controllers/RENCONTREController.php
+++ app/Http/Controllers/RENCONTREController.php
@@ -9,42 +9,25 @@ use Illuminate\Support\Facades\Log;
 
 class RENCONTREController extends Controller {
 
-// public function ListeRencontres(Request $request) {
-
-//     $rencontre = Rencontre::select('idrencontre','jeux','equipe1','equipe2', 'daterenc', 'equipe_gagnante')->first();
-//     $equipes=array();    
-//     Log::info("Categorie: " . $rencontre);
-//     // foreach($rencontre as $renc) {
-
-//             $equipes = Equipe::all()->where("idequipe", "=",$rencontre->equipe2);
-//         // }
-        
-
+    public function ListeRencontres(Request $request) {
+        $rencontre = Rencontre::select('idrencontre','jeux','equipe1','equipe2', 'daterenc', 'equipe_gagnante')->first();
+        $equipe1 = Equipe::where("idequipe", "=", $rencontre->equipe1)->get();
+        $equipe2 = Equipe::where("idequipe", "=", $rencontre->equipe2)->get();
+        $equipeGagnante = Equipe::where("idequipe", "=", $rencontre->equipe_gagnante)->get();
     
+        $equipes = collect([
+            "equipe1" => $equipe1,
+            "equipe2" => $equipe2,
+            "equipe_gagnante" => $equipeGagnante,
+        ]);
     
-//     return response()->json(["rencontre"=>$rencontre, "equipes"=>$equipes]);
-// }
-
-public function ListeRencontres(Request $request) {
-    $rencontre = Rencontre::select('idrencontre', 'jeux', 'equipe1', 'equipe2', 'daterenc', 'equipe_gagnante')->first();
-
-    $equipes1 = Equipe::all()->where('idequipe', '=', $rencontre->equipe1);
-    $equipes2 = Equipe::all()->where('idequipe', '=', $rencontre->equipe2);
-    $equipesGagnante = Equipe::all()->where('idequipe', '=', $rencontre->equipe_gagnante);
-
-    $equipes = array();
-    foreach ($equipes1 as $equipe) {
-        $equipes[] = $equipe->nom;
-    }
-    foreach ($equipes2 as $equipe) {
-        $equipes[] = $equipe->nom;
-    }
-    foreach ($equipesGagnante as $equipe) {
-        $equipes[] = $equipe->nom;
-    }
+        foreach ($equipes as $key => $equipe) {
+            Log::info("Equipe " . $key . ": " . $equipe);
+        }
     
-    return response()->json(["rencontre"=>$rencontre, "equipes"=>$equipes]);
-}
+        return response()->json(["rencontre" => $rencontre, "equipes" => $equipes]);
+    }
+
 
 public function AjouterRencontre(Request $request) {
 
